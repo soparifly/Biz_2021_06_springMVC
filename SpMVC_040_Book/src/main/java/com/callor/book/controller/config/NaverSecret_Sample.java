4192c87cc4ca031105fd4b91d901f39eb2133fb6
soparifly <soparifly@naver.com>
2021-06-28 15:18:02 +0900

    sample

---
+++ SpMVC_040_Book/src/main/java/com/callor/book/controller/config/NaverSecret_Sample.java
@@ -0,0 +1,7 @@
+package com.callor.book.controller.config;
+
+public class NaverSecret_Sample {
+
+	public static final String NAVER_CLIENT_ID="";
+	public static final String NAMVER_CLIENT_SECRET="";
+}
