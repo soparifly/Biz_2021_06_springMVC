76f821f50e609242ef6d0ad4c137bd9326e5f494
soparifly <soparifly@naver.com>
2021-08-13 21:35:31 +0900

    team

---
+++ Universe/src/main/java/com/usiverce/sopar/config/AstroSecret.java
@@ -0,0 +1,11 @@
+package com.usiverce.sopar.config;
+
+public class AstroSecret {
+
+	
+
+	public final static String astro_event_url = "http://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo";
+	public final static String astro_event_service_key = "";
+
+	
+}
